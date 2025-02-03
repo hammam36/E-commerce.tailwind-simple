8a9c3b95576bc74b1727db38635e657eb1bb4fd8
همام مبارك <hammam1dn@gmail.com>
2025-02-03 09:04:56 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: ["./*.html", "./src/**/*.js"],
+    theme: {
+      extend: {
+        colors: {
+          warnaBiru: '#07A3ED', 
+        },
+      },
+    },
+    plugins: [],
+  };
